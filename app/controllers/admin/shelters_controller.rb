60bf9db82aad576497bcbe56730b1dfb1efa5e0e
Meredith Trevino <meredithtrevino3@gmail.com>
2023-08-28 15:34:25 -0600

    adds image file

--- app/controllers/admin/shelters_controller.rb
+++ app/controllers/admin/shelters_controller.rb
@@ -2,6 +2,7 @@ class Admin::SheltersController < ApplicationController
 
   def index
     @shelters = Shelter.find_by_sql("SELECT name FROM shelters ORDER BY name DESC")
+    #MVC
     @shelters_pending_apps = Shelter.list_pending_apps
   end
 end
